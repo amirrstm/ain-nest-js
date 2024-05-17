800b36dbcc7535e07eb8b0f075887c86f98f41c5
Amir Rostami <108669687+amirnomysh@users.noreply.github.com>
2024-05-17 20:43:46 +0330

    Added Data Entry

--- src/modules/user/dtos/user.prompt.image.dto.ts
+++ src/modules/user/dtos/user.prompt.image.dto.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsUUID } from 'class-validator'
+import { IsNotEmpty } from 'class-validator'
 
 export class UserImagePromptDto {
   @ApiProperty({
